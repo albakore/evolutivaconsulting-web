e8a0bbf25116d11b452f4cba2842754660cdf53c
Leandro Silva <leeansilva@gmail.com>
2023-09-28 20:31:33 -0300

    clients

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import Seccion from "@/components/seccion/Seccion"
 import Footer from "@/components/footer/Footer"
 import { Servicios } from "@/components/servicios/Servicios"
 import { Contacto } from "@/components/contacto/Contacto"
+import Clientes from "@/components/clientes/Clientes"
 
 export default function Home() {
   return (
@@ -67,7 +68,7 @@ export default function Home() {
       
       <Divider borderColor='gray.300' />
       
-      <Contacto></Contacto>
+      <Clientes/>
       <Footer />
       
     </Box>
