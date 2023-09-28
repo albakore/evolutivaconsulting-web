ebce77da5334ca5b8ae6dac947f1f26f4f53f582
Leandro Silva <leeansilva@gmail.com>
2023-09-28 20:31:33 -0300

    clients

--- src/components/scroll/ImageScroll.jsx
+++ src/components/scroll/ImageScroll.jsx
@@ -1,8 +1,8 @@
 import { Flex, Image, Box, keyframes, HStack, VStack } from '@chakra-ui/react'
 import Seccion from '../seccion/Seccion';
 import React from 'react'
-
 export default function ImageScroll() {
+	const [isHover, setHover] = React.useState(false)
 
 	const animationKeyframes = keyframes`
 	0% { transform: translateX(0%); }
@@ -27,20 +27,24 @@ export default function ImageScroll() {
 	})
   
 	return (
-	  <Seccion id="contacto">
-		<HStack overflow={'hidden'}  gap={0}>
-			<HStack gap={0} flexShrink={0} animation={animation}>
-				{lista}
-			</HStack>
-			<HStack gap={0} flexShrink={0} animation={animation}>
-				{lista}
-			</HStack>
-			<HStack gap={0} flexShrink={0} animation={animation}>
-				{lista}
-			</HStack>
-			
-		</HStack>
+	  	<Seccion id="contacto">
+		
+				<HStack gap={0} position={'relative'} overflow={'hidden'}  >
+					<Box pointerEvents={'none'} borderRadius={'3px'} w='100%' h={'100%'} top={0} left={0} zIndex={200} position={'absolute'} background={'linear-gradient(to right, #ffff, transparent, transparent, #ffff)'} boxShadow='0 0 15px 15px white inset'></Box>
+					
+					<HStack onMouseLeave={()=>{setHover(false)}} onMouseEnter={()=>{setHover(true)}} gap={0} flexShrink={0} animation={animation} sx={isHover && {animationPlayState:'paused'}}>
+						{lista}
+					</HStack>
+					<HStack onMouseLeave={()=>{setHover(false)}} onMouseEnter={()=>{setHover(true)}} gap={0} flexShrink={0} animation={animation} sx={isHover && {animationPlayState:'paused'}}>
+						{lista}
+					</HStack>
+					<HStack onMouseLeave={()=>{setHover(false)}} onMouseEnter={()=>{setHover(true)}} gap={0} flexShrink={0} animation={animation} sx={isHover && {animationPlayState:'paused'}}>
+						{lista}
+					</HStack>
+					
+				</HStack>
+
   
-	  </Seccion>
+	  	</Seccion>
 	);
   };
