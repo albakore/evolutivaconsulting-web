72d8091ae7cf5d24aeebfa8805a09355575e7b81
Leandro Silva <leeansilva@gmail.com>
2023-10-02 20:03:31 -0300

    background move

--- src/components/background/DotBackground.jsx
+++ src/components/background/DotBackground.jsx
@@ -1,37 +1,42 @@
-import React from 'react'
-import estilo from './dotBackground.module.css'
+import React from 'react';
+import estilo from './dotBackground.module.css';
 
 const backgroundDot = {
-    width: '1vmin',
-    height: '1vmin',
-    borderRadius: '1vmin',
-    backfaceVisibility: 'hidden',
-    position: 'absolute',
-    animation: 'move',
-    animationDuration: 15,
-    animationTimingFunction: 'linear',
-    animationIterationCount: 'infinite',
-}
+  width: '1vmin',
+  height: '1vmin',
+  borderRadius: '1vmin',
+  backfaceVisibility: 'hidden',
+  position: 'absolute',
+  animation: 'move',
+  animationDuration: 15,
+  animationTimingFunction: 'linear',
+  animationIterationCount: 'infinite',
+};
 
-export default function DotBackground({cantidad = 20}) {
+export default function DotBackground({ cantidad = 20 }) {
+
+  const puntos = Array.from({ length: cantidad }, (_, index) => index);
+
   return (
-	<div class="background">
-		{
-			Array.from(cantidad).forEach(punto => {
-				estilo = {
-					color: '#282c7d',
-					top: '77%',
-					left: '44%',
-					animationDuration: '38s',
-					animationDelay: '-314s',
-					transformOrigin: '9vw -5vh',
-					boxShadow: '-2vmin 0 0.7401954773023416vmin currentColor',
-				}
-				return (
-					<span className={css.dotBackground} style={estilo}></span>
-				)
-			})
-		}
-	</div>
-  )
+    <div className={estilo.background}>
+      {puntos.map((punto, index) => {
+		const numRandom = Math.random() * 50
+        const estiloPunto = {
+		  width: `${numRandom}vmin`,
+		  height: `${numRandom}vmin`,
+		  borderRadius: `${numRandom}vmin`,
+          color: '#bee3f8',
+          top:`${Math.random()*100}%`,
+          left: `${Math.random()*100}%`,
+          animationDuration: `${Math.random()*100}%`,
+          animationDelay: `${-314 * index}s`, // Ajustar el retardo para cada punto
+          transformOrigin: '9vw -5vh',
+          boxShadow: `-50vmin 0 1.7401954773023416vmin currentColor`,
+		  opacity: '0.2'
+        };
+
+        return <span className={estilo.dotBackground} style={estiloPunto} key={index}></span>;
+      })}
+    </div>
+  );
 }
