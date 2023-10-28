36358ed859c2fa736193d6d685a70e332d2413f5
Kevin Kener <kevink.contacto@gmail.com>
2023-10-28 11:50:17 -0300

    Ajuste de textos para mobiles

--- src/components/seccion/Servicios.jsx
+++ src/components/seccion/Servicios.jsx
@@ -14,8 +14,8 @@ export default function ItemServicio({numero,titulo, children, color}) {
 		>
 
           <HStack alignItems={'end'} justifyContent={{base:'start',lg:'start'}}  w={{base:'100%',lg:'50%'}}>
-            <Text fontSize={{base:'4rem',md:'5rem',lg:'8rem'}} letterSpacing={{base:0, md:-15}} fontWeight={200} lineHeight={'107px'} color={'blue.300'} textShadow={'0 4px 10px'} mr={{base:2, md: 5}}>{numero}</Text>
-            <Text fontSize={{base:'30px', md:'3xl',lg:'4xl'}} fontWeight={600} lineHeight={'40px'} textTransform={'uppercase'}>{titulo}</Text>
+            <Text fontSize={{base:'3rem',md:'5rem',lg:'8rem'}} letterSpacing={{base:0, md:-15}} fontWeight={200} lineHeight={{base:'55px',md:'107px'}} color={'blue.300'} textShadow={'0 4px 10px'} mr={{base:2, md: 5}}>{numero}</Text>
+            <Text fontSize={{base:'28px', md:'3xl',lg:'4xl'}} fontWeight={600} lineHeight={'40px'} pr='10px' textTransform={'uppercase'}>{titulo}</Text>
           </HStack>
 
           <VStack gap={0} fontSize={'xl'} justifyContent={{base:'center',lg:'start'}} alignItems={'start'} w={{base:'100%', lg:'50%'}} paddingInline={10} fontWeight={400} divider={<StackDivider borderColor='cyan.900' />}>
