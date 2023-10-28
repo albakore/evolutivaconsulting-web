0baa5e4a5259e3a8fa3b4622eb5f379ab1bfbd4c
Kevin Kener <kevink.contacto@gmail.com>
2023-10-28 11:50:45 -0300

    Se agrega a Tomas a la seccion 'Quienes somos'

--- src/components/quienesSomos/QuienesSomos.jsx
+++ src/components/quienesSomos/QuienesSomos.jsx
@@ -9,13 +9,28 @@ export default function QuienesSomos() {
           <Text fontSize={'4xl'} fontWeight={800} lineHeight={'40px'} textTransform={'uppercase'} textAlign={'center'} mb={10}>
           Quienes somos
           </Text>
-          <Flex justifyContent={{base:'start', md:'center'}} alignItems={{base:'inherit', md:'center'}} direction={{base:'column', sm:'row'}} gap={10}>
-            <Image src="integrantes/Nico.jpeg" alt='Nicolas Cichocki' w={300} borderRadius={20} border={'1px solid'} boxShadow={'dark-lg'} />
-            <VStack alignItems={'start'}>
+          <Text textAlign={'center'}>Tomás y Nicolás tienen más de 10 años de experiencia en campañas políticas, marketing y comunicación estratégica. </Text>
+
+          <Flex direction={{base:'column', md:'row'}}>
+
+          <Flex justifyContent={{base:'start', md:'center'}} alignItems={{base:'inherit', md:'center'}} direction={{base:'column', md:'column'}} gap={10} p={10}>
+            <Image src="integrantes/Nico.jpeg" alt='Nicolas Cichocki' w={300} borderRadius={300} border={'1px solid white'} boxShadow={'dark-lg'} />
+            <VStack alignItems={'center'}>
               <Text fontSize={'3xl'} fontWeight={400}>Nicolas Cichocki</Text>
-              <Text>Tomás y Nicolás tienen más de 10 años de experiencia en campañas políticas, marketing y comunicación estratégica. </Text>
+              
             </VStack>
           </Flex>
+
+          <Flex justifyContent={{base:'start', md:'center'}} alignItems={{base:'inherit', md:'center'}} direction={{base:'column', md:'column'}} gap={10}  p={10}>
+            <Image src="integrantes/Tomas.jpg" alt='Tomás Suarez' w={300} borderRadius={300} border={'1px solid white'} boxShadow={'dark-lg'} />
+            <VStack alignItems={'center'}>
+              <Text fontSize={'3xl'} fontWeight={400}>Tomás Suarez</Text>
+              
+            </VStack>
+          </Flex>
+
+          </Flex>
+
         </VStack>
 		
 	</Seccion>
