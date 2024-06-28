17d53059337b9006067bf474c76eb8babfcbece5
albakore <albakoreofficial@gmail.com>
2024-06-28 16:14:10 -0300

    se agregar GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager,GoogleAnalytics } from '@next/third-parties/google'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-        <GoogleTagManager gtmId="G-FYFEN1LWGW" />
+        <GoogleAnalytics gaId="G-FYFEN1LWGW" />
       <head>
         <title>Evolutiva Consulting</title>
       </head>
