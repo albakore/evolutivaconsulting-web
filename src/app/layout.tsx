1ebe0fddda524d988b9f8651cd7fc940f52e8206
albakore <albakoreofficial@gmail.com>
2024-06-28 16:28:22 -0300

    gtmevent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import { GoogleTagManager,GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager,GoogleAnalytics, sendGTMEvent } from '@next/third-parties/google'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -8,9 +8,12 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }) {
+    sendGTMEvent({
+        event: 'page_view',
+    })
   return (
     <html lang="es">
-        <GoogleAnalytics gaId="G-FYFEN1LWGW" />
+        <GoogleTagManager gtmId="G-FYFEN1LWGW" />
       <body>
       {children}
         
